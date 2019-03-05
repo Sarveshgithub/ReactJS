c3799e821984b1e6bb8990fdc41ca20802267762
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-03-05 14:48:51 +0530

    add files

--- react-basics/src/components/header.js
+++ react-basics/src/components/header.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Header = () => {
-    return <div>Header</div>
+class Header extends Component {
+    //contructors 
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'keywords: ',
+            keywords: ''
+        }
+    }
+    //onchange events
+    onchange(event) {
+        this.setState({ keywords: event.target.value })
+        console.log('values::', event.target.value);
+    }
+    //render functions
+    render() {
+        return (
+            <header>
+                <div className="logo">
+                    Logo
+                </div>
+                <input onChange={this.onchange.bind(this)} />
+                <div>
+                    {this.state.title}
+                    {this.state.keywords}
+                </div>
+            </header>
+        )
+    }
 }
+
 export default Header;
