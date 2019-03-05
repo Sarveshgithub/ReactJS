0afeccb0ce5bb5f0b6a0c24f5e9a8ca7a84c304a
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-03-05 12:16:55 +0530

    add components

--- react-basics/src/index.js
+++ react-basics/src/index.js
@@ -1,1 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
+
+//Components import from components folders
+import Header from './components/header'
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>This my first App</h1>
+        </div>
+    )
+}
+ReactDOM.render(<App />, document.querySelector('#root'));
