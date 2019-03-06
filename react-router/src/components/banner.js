65408ee60d12991dd5ea0e1a200659ffdccb5974
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-03-06 16:01:47 +0530

    add css

--- react-router/src/components/banner.js
+++ react-router/src/components/banner.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import Banner from '../components/banner'
+
 class Home extends Component {
     
     render() {
         return (
             <div>
-                <Banner />
+                Home
              </div>
         )
     }
