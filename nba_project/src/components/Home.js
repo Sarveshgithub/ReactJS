57f7c592a4a5cac31d4e5666eb14981f8d580564
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-03-10 14:46:22 +0530

    slder

--- nba_project/src/components/Home.js
+++ nba_project/src/components/Home.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
-
+//Components
+import Featured from './featured'
+const URL_HOME = 'http://localhost:3004/home'
 class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            home: ''
+        }
+    }
+    componentDidMount() {
+        fetch(URL_HOME, { method: 'GET' })
+            .then(response => response.json())
+            .then(json => {
+                this.setState({ home: json });
+            })
+    }
     render() {
         return (
             <div>
-                Home
+                <Featured slides={this.state.home.slider}/>
             </div>
         )
     }
